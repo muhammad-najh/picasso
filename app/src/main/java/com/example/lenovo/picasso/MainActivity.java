986cd77b91a4be7ecb74e4e26515332ea61227f4
muhamad najh <muhammadnajh98@gmail.com>
2018-08-12 16:48:26 +0300

    some coment

---
+++ app/src/main/java/com/example/lenovo/picasso/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.lenovo.picasso;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
